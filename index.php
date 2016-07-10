2a4b24547cfe5cdeaf1ac93faf2027e81c2ca8ad
Blackbird73 <vince.willems@gmail.com>
2016-07-10 23:02:04 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+// Page index : redirection de liens ou redirection vers site web
+ 
+    require_once('inc/config.inc.php'); // Variables
+ 
+    if(isset($_GET['id'])){
+        // Eviter les mauvaises surprises
+        $id = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['id']);   
+
+        if( file_exists($chemin.$id.$ext) ){ // le fichier existe-t-il ?
+            // OUI : on poursuit la procédure
+
+            $fichier = fopen($chemin.$id.$ext, "r");
+            while(!feof($fichier)){
+                $lien   = urldecode(fgets($fichier));
+                $option = substr(fgets($fichier),-1);
+            }
+            fclose($fichier);
+
+           if ($option == 1) { // $option == 0 --> infini
+            unlink($chemin.$id.$ext); // Supprimer le fichier si option = 1
+            } else {
+                if ($option !=0) {
+                    $option = $option -1; // retrancher 1
+                    $fichier = fopen($chemin.$id.$ext, "w");
+                        fputs ($fichier, $lien);
+                        fputs ($fichier, $option);
+                    fclose($fichier);
+                }
+            } 
+
+            // Stats : enregister une trace du passage dans un fichier
+                // $fic = $chemin.$id.$sta;
+                // $fp = fopen($fic, "r"); 
+                // $count = fread($fp, 1024); 
+                // fclose($fp); 
+                // $fp = fopen($sta, "w"); 
+                // fwrite($fp, $count); 
+                // fclose($fp); 
+                $fic = $chemin.$id.$sta;
+                $fp = fopen ($fic, "r+"); // w+ : ouvre et écrit, s'il n'existe pas, il le crée
+                $hits = fread ($fp, 1024);
+                $hits = $hits +1;
+                fseek ($fp, 0);
+                fwrite ($fp, $hits);
+                fclose ($fp);
+
+            header('location: '.$lien); // on redirige vers le lien long
+
+        } else {
+            // NON : Lien introuvable : on redirige vers la sortie...
+            header('location: '.$accueil); 
+        }
+ 
+    } else {
+        // Aucun ID : on redirige vers la sortie...
+        header('location: '.$accueil);
+    }
+ 
+?>
